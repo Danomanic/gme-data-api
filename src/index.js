3accb756b445dd2b1aa7668f772a81150efd78dc
daniel.pomfret <daniel.pomfret@dwp.gov.uk>
2020-02-28 10:38:36 +0000

    Add AUTHKEY

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ const routes = require('./routes');
 
 const { log } = console;
 
-app.use('/', routes);
-
 const PORT = process.env.PORT || 3000;
 
+const auth = (req, res, next) => {
+  if (req.headers.authkey !== null && req.headers.authkey === process.env.AUTHKEY) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+app.use(auth);
+app.use('/', routes);
 app.listen(PORT, () => log(`API Running on: ${PORT}`));
